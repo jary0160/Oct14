df4468d1afa42f24e1f5ca5817750618eabc9fa5
bsamra <bsamra@TNB80R9M52.scdsb.pri>
2020-10-14 00:07:40 -0400

    almost done 6

---
+++ src/PatronBook/Book.java
@@ -0,0 +1,19 @@
+
+package PatronBook;
+
+
+
+
+
+public class Book {
+    private Book(String author, String title){
+        }
+        public String getTitle(String title){
+               return title;
+        }
+        public String toString(){
+        
+        return title + " " + author + " ";
+            }
+
+}
